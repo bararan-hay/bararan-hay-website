b1953737e6afedd4e61192f1e48d8e7b7f0eb54e
Tigran <tigran.contact@gmail.com>
2020-04-19 19:56:36 +0400

    update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,57 +1,75 @@
-var dictionary = null;
-window.axios
-	.get('https://raw.githubusercontent.com/tigransimonyan/baratian-dictionary-assets/master/baratyan-dictionary.tab')
-	.then(function (response) {
-		dictionary = response.data
-	})
-
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function () {
-		var context = this, args = arguments;
-		var later = function () {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+var dictionaries = [{
+	name: "Բարաթյանի բառարան",
+	pattern: "^text.+\n",
+	link: "https://github.com/tigransimonyan/baratian-dictionary-assets",
+	row: "https://raw.githubusercontent.com/tigransimonyan/baratian-dictionary-assets/master/baratyan-dictionary.tab"
+}, {
+	name: "noch տեխնիկական բառարան",
+	pattern: "^text.+\n",
+	link: "https://github.com/norayr/noch-armenian-dictionary",
+	row: "https://raw.githubusercontent.com/norayr/noch-armenian-dictionary/master/noch_en-hy.tab"
+}];
 
 var loading = false;
-var searchAndRender = debounce(function (regex) {
-	var array = dictionary.match(new RegExp(regex, "gim"));
-	var html = ''
-	if (array) {
-		html += '<ul class="pl-3">'
-		array.forEach(function (item) {
-			html += '<li class="mt-2">' + item + '</li>'
+var timeout = null;
+
+dictionaries.forEach(dictionary => {
+	dictionary.data = "";
+	window.axios
+		.get(dictionary.row)
+		.then(function (response) {
+			dictionary.data = response.data
 		})
-		html += '</ul>'
-	} else {
-		html += '<h6 class="text-center mt-5">Շտեմարանում նման բառ չկա ։(</h6>'
+})
+
+function getLoadingHtml() {
+	var html = '<div class="d-flex justify-content-center mt-5">';
+	html += '<div class="spinner-border text-dark" role="status">';
+	html += '<span class="sr-only">որոնում...</span>';
+	html += '</div>';
+	html += '</div>';
+	return html;
+}
+
+function getResultsHtml(dictionary, array) {
+	if (!array || array.length === 0) {
+		return '';
 	}
-	results.innerHTML = html;
-	loading = false;
-}, 250);
+	var html = '<div class="border-bottom pb-2 pt-4 d-flex flex-row justify-content-between">';
+	html += '<b>' + dictionary.name + ' (' + array.length + ' բառ) </b>';
+	html += '<a target="_blank" href="' + dictionary.link + '">աղբյուր</a>';
+	html += '</div>'
+	array.forEach(function (item) {
+		html += '<li class="mt-2">' + item + '</li>'
+	});
+	html += '</ul>'
+	return html;
+}
+
+function renderResultsHtml(html) {
+	return html || '<h6 class="text-center mt-5">Շտեմարանում նման բառ չկա ։(</h6>';
+}
 
 function handleInput(e) {
+	clearTimeout(timeout)
 	var text = e.value.trim();
-	var regex = "^" + text + ".+\n";
-	if (!text || !dictionary) {
+	if (!text) {
 		results.innerHTML = "";
 		return;
 	}
 	if (!loading) {
-		var html = '<div class="d-flex justify-content-center mt-5">';
-		html += '<div class="spinner-border text-dark" role="status">';
-		html += '<span class="sr-only">որոնում...</span>';
-		html += '</div>';
-		html += '</div>';
-		results.innerHTML = html;
+		results.innerHTML = getLoadingHtml();
 		loading = true;
 	}
-	searchAndRender(regex)
+	timeout = setTimeout(function () {
+		var html = '';
+		dictionaries.forEach(dictionary => {
+			var pattern = dictionary.pattern.replace("text", text);
+			var regex = new RegExp(pattern, "gim");
+			var results = dictionary.data.match(regex);
+			html += getResultsHtml(dictionary, results);
+		})
+		results.innerHTML = renderResultsHtml(html);
+		loading = false;
+	}, 300)
 }
