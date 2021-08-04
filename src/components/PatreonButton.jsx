f4451102f780c37dd5fee4571c4807b0845f0ba8
Tigran Simonyan <tigran@frontjet.com>
2021-08-04 16:49:56 +0400

    update

--- src/components/PatreonButton.jsx
+++ src/components/PatreonButton.jsx
@@ -1,17 +1,9 @@
-import { Button } from 'antd';
 import patreon from 'assets/images/patreon-w.png';
 
 export default function PatreonButton() {
   return (
-    <Button
-      style={{ backgroundColor: 'rgb(255, 66, 77)', color: 'white' }}
-      type="link"
-      shape="round"
-      target="_blank"
-      icon={<img width={16} src={patreon} style={{ marginRight: 8 }} />}
-      href="https://www.patreon.com/bePatron?u=41176574"
-    >
-      Սատարել
-    </Button>
+    <a target="_blank" href="https://www.patreon.com/bePatron?u=41176574">
+      <img width={127} src={patreon} />
+    </a>
   );
 }
